e086af79ab974e53cd6fd944f2483ec03f9fc51f
GeorgeDev012 <jerzytuszynski012@gmail.com>
2018-11-22 12:54:34 +0100

    Service injcetion and small changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,33 @@
 import { Component } from '@angular/core';
-import { ThoughtsService } from './thoughts.service'
+import { ThoughtsService } from './thoughts.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   public text: String = "szczerozłota myśl"
   public thoughts: Array<String> = []
+
+  constructor(
+    private thoughtsService: ThoughtsService
+  ) {}
   
   public addThought(): void {
     this.thoughts.push(this.text)
-    console.log(this.thoughts)
     this.text = ""
-    localStorage.setItem("thoughtsList", JSON.stringify(this.thoughts))
+    this.thoughtsService.saveThoughts(this.thoughts)
   }
 
   public removeThought(i): void {
     this.thoughts.splice(i,1)
-    
+    this.thoughtsService.saveThoughts(this.thoughts)
+  }
+
+  private ngOnInit() {
+    this.thoughts = this.thoughtsService.fetchThoughts()
   }
 }
--- src/app/thoughts.service.ts
+++ src/app/thoughts.service.ts
@@ -5,11 +5,14 @@ import { Injectable } from '@angular/core';
 })
 export class ThoughtsService {
 
-  constructor(
-    private thoughtsService: ThoughtsService
-  ) { }
+  constructor() {}
 
   public saveThoughts(thoughts: Array<String>): void {
     localStorage.setItem("thoughtsList", JSON.stringify(thoughts))
+  }
+
+  public fetchThoughts(): Array<String> {
+    let data = JSON.parse(localStorage.getItem("thoughtsList"))
+    return data ? data : []
   }
 }
